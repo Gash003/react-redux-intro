6cba51cfc8a2301b99a75fe4e2a5bef083dea962
MPawluk <m.blizniak@gmail.com>
2017-01-26 08:49:21 +0100

    GET API endpoints are added

--- src/server/server.js
+++ src/server/server.js
@@ -2,15 +2,25 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const userList = require('./data/users.json');
+const widgetList = require('./data/widgets.json');
 
 const publicPath = path.join(__dirname, '..', 'client', 'public');
-const indexFile = path.resolve(__dirname, '..', 'client', 'index.html');
+const indexFile = path.join(__dirname, '..', 'client', 'index.html');
 
 app.use(express.static(publicPath, {
   dotfiles: 'ignore',
   index: false
 }));
 
+
+app.get('/users', function(req, res) {
+  res.json(userList);
+});
+
+app.get('/widgets', function(req, res) {
+  res.json(widgetList);
+});
 
 app.get('*', function(req, res, next) {
   console.log('Request: [GET]', req.originalUrl);
